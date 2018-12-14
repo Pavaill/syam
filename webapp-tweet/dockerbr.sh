f021fb4ed5022a8e2ab983b9c68f89b8d1a4175a
AkioHSS <akiohoess@gmail.com>
2018-12-14 11:57:45 +0100

    token

--- webapp-tweet/dockerbr.sh
+++ webapp-tweet/dockerbr.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+mvn clean package
+
 sudo docker build --tag=webapp-tweet .
-sudo docker run -it --rm -p 8080:8080 -p 9990:9990 -p 8787:8787 -v /home/twitter/Bureau/syam/webapp-tweet/target:/opt/jboss/wildfly/standalone/deployments/ webapp-tweet
